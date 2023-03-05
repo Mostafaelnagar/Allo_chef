5ab32b4c934a477c35e906398832c8d01af0499c
Mostafa <m.elnaggar>
2023-03-05 04:50:06 +0200

    order details

--- app/src/main/java/app/te/alo_chef/data/my_orders/data_source/OrdersServices.kt
+++ app/src/main/java/app/te/alo_chef/data/my_orders/data_source/OrdersServices.kt
@@ -1,7 +1,6 @@
 package app.te.alo_chef.data.my_orders.data_source
 
 import app.te.alo_chef.data.my_orders.dto.MyOrdersData
-import app.te.alo_chef.data.wallet.dto.TransActionsHistoryItem
 import app.te.alo_chef.domain.utils.BaseResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,7 +9,6 @@ interface OrdersServices {
     @GET("user/my-orders")
     suspend fun getOrders(@Query("type") type: String): BaseResponse<List<MyOrdersData>>
 
-    @GET("user/order_details")
+    @GET("user/get-order-details")
     suspend fun getOrderDetails(@Query("order_id") order_id: Int): BaseResponse<MyOrdersData>
-
 }
